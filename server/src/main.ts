fe18f6a69efcd6dee28462b54a40f8007d663a98
Thevindu Senanayake <thevindusenanayake76@gmail.com>
2026-06-23 03:56:05 +0530

    chore(server): add helmet middleware for security headers
    Helmet sets HTTP security headers (X-Frame-Options, CSP, HSTS, etc.)
    to protect against common web vulnerabilities. Registered globally
    at the start of the middleware stack.
    
    Signed-off-by: Thevindu Senanayake <thevindusenanayake76@gmail.com>

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,16 +1,22 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
+import express from 'express';
 import { AppModule } from './app.module.js';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter.js';
 import { EventLoggerInterceptor } from './common/interceptors/event-logger.interceptor.js';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.use(helmet());
+    app.use(express.json({ limit: '5mb' }));
+    app.use(express.urlencoded({ limit: '5mb', extended: true }));
     app.setGlobalPrefix('api');
     app.enableCors({ origin: true, credentials: true });
     app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
     app.useGlobalFilters(new HttpExceptionFilter());
     app.useGlobalInterceptors(new EventLoggerInterceptor());
-    await app.listen(process.env.PORT ?? 3000);
+    const server = await app.listen(process.env.PORT ?? 3000);
+    server.requestTimeout = 30000;
 }
 bootstrap();
