f4c59778e98716814f7c0473682816037f3c2b92
Thevindu Senanayake <thevindusenanayake76@gmail.com>
2026-06-22 15:56:45 +0530

    feat(server): register BookingModule and WaitlistModule in AppModule
    Signed-off-by: Thevindu Senanayake <thevindusenanayake76@gmail.com>

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,11 +2,19 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
-import { PrismaModule } from './prisma/prisma.module.js';
 import { AuthModule } from './auth/auth.module.js';
+import { BookingModule } from './booking/booking.module.js';
+import { PrismaModule } from './prisma/prisma.module.js';
+import { WaitlistModule } from './waitlist/waitlist.module.js';
 
 @Module({
-    imports: [ConfigModule.forRoot({ isGlobal: true }), PrismaModule, AuthModule],
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
+        PrismaModule,
+        AuthModule,
+        BookingModule,
+        WaitlistModule,
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
