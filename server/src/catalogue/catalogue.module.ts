e764c930ca8f9fee32f9f9fc44e1e4b63462a1ec
Thevindu Senanayake <thevindusenanayake76@gmail.com>
2026-06-22 17:54:57 +0530

    feat(catalogue): add CatalogueModule scaffold
    Signed-off-by: Thevindu Senanayake <thevindusenanayake76@gmail.com>

---
+++ server/src/catalogue/catalogue.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { BookController } from './controllers/book.controller.js';
+import { CategoryController } from './controllers/category.controller.js';
+import { DeviceController } from './controllers/device.controller.js';
+import { RoomController } from './controllers/room.controller.js';
+import { BookService } from './services/book.service.js';
+import { CategoryService } from './services/category.service.js';
+import { DeviceService } from './services/device.service.js';
+import { RoomService } from './services/room.service.js';
+
+@Module({
+    controllers: [CategoryController, BookController, DeviceController, RoomController],
+    providers: [CategoryService, BookService, DeviceService, RoomService],
+})
+export class CatalogueModule {}
