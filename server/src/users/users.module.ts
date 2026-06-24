eec07c0d734e22096ce57f8d64af99979fa5ccba
Thevindu Senanayake <thevindusenanayake76@gmail.com>
2026-06-25 01:10:52 +0530

    feat(users): make create/update use dynamic tier thresholds
    - Inject SystemConfigService into UsersService
    - Make create() async to fetch dynamic tier config
    - Use tierFromPointsWithConfig() with dynamic thresholds
    - Derive tier from config when setting explicit tier
    - Add SystemConfigModule import to UsersModule
    
    Signed-off-by: Thevindu Senanayake <thevindusenanayake76@gmail.com>

--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { SystemConfigModule } from '../config/system-config.module.js';
 import { UsersController } from './users.controller.js';
 import { UsersService } from './users.service.js';
 
 @Module({
+    imports: [SystemConfigModule],
     controllers: [UsersController],
     providers: [UsersService],
     exports: [UsersService],
